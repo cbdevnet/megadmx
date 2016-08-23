22afadf41f5c3286f4c03dccfe1529ad07f119d3
cbdev <cb@cbcdn.com>
2016-08-23 23:52:42 +0200

    Functional t13 receiver

--- tinyrecv/receiver.s
+++ tinyrecv/receiver.s
@@ -3,10 +3,11 @@
 .org 0
 rjmp setup
 
-.EQU CHANNELS = 2
-.EQU ADDRESS = 28
+.EQU ADDRESS1 = 301
+.EQU ADDRESS2 = 512
 .EQU DMXPIN = 3
 ; 9.6 cycles / usec
+; TODO simple address setting (press button, set channel)
 
 setup:
 		cli
@@ -28,38 +29,41 @@ setup:
 		out TCCR0B, r16
 		sei
 
-		ldi r16, 200
+		; Initial PWM values
+		ldi r16, 0
 		out OCR0A, r16
-
-		ldi r16, 100
+		ldi r16, 0
 		out OCR0B, r16
 
+		; Initialize register constants
 		ldi r16, (1 << DMXPIN)
 		mov r14, r16
 
-		ldi r16, 0b00101000
-		mov r22, r16
-		ldi r16, 0b00111000
-		mov r23, r16
-
+		; Load local addresses
+		ldi r26, low(ADDRESS1)
+		ldi r27, high(ADDRESS1)
+		ldi r28, low(ADDRESS2)
+		ldi r29, high(ADDRESS2)
 
 		; Run the main loop
 		rjmp main
 
 ; 	Production main loop
 main:
-		ldi r16, 104		; ceil(88 usec * 9.6 cycles) / 8  +- 4
+		ldi r16, 104			; ceil(88 usec * 9.6 cycles) / 8  +- 4
+main_rescan:
 		rcall scan_break
 main_reent:
 		; TODO verify MARK
 		; In MAB
-		;sbi PORTB, 4
-		;cbi PORTB, 4
+		; Load current address
+		ldi r25, 0
+		ldi r24, 0
 		; Fall through
 main_read_byte:
 		; Wait for startbit
-		sbic PINB, DMXPIN	; 1C false 2 true
-		rjmp main_read_byte	; 2C
+		sbic PINB, DMXPIN		; 1C false 2 true
+		rjmp main_read_byte		; 2C
 		; Now in startbit (min 2C max 4C)
 		ldi r16, 0
 		ldi r18, 0xFF
@@ -100,27 +104,90 @@ main_read_byte_sample_bit:
 		nop
 		nop
 
-		sbi PORTB, 4
-		cbi PORTB, 4
-		;nop
-		;nop
-		;nop
-		;nop
+		;sbi PORTB, 4
+		;cbi PORTB, 4
+		nop
+		nop
+		nop
+		nop
 
 		sbrs r18, 3
 		rjmp main_read_byte_sample_bit
-		; TODO sample stopbit 1, process
-		sbi PORTB, 4
-		nop
-		nop
-		nop
-		nop
-		cbi PORTB, 4
-		sbis PINB, DMXPIN		; Assert stop bit 1 or scan for new break ; FIXME do RESCAN instead of SCAN
-		rjmp main
-		rjmp main_read_byte
-		;rjmp main
+main_byte:
+		com r16				; Somehow the data byte is inverted
+		; Check if startcode
+		ldi r18, 0
+		cpi r24, 0
+		cpc r25, r18
+		breq main_byte_startcode
 
+		; Decode data
+		ror r16
+		rol r17
+		ror r16
+		rol r17
+		ror r16
+		rol r17
+		ror r16
+		rol r17
+		ror r16
+		rol r17
+		ror r16
+		rol r17
+		ror r16
+		rol r17
+		ror r16
+		rol r17
+
+		; Check local addresses for match
+		cp r24, r26
+		cpc r25, r27
+		breq main_byte_a1
+		cp r24, r28
+		cpc r25, r29
+		breq main_byte_a2
+
+
+		; Do nothing
+		rjmp main_byte_done
+
+main_byte_startcode:
+		; If startcode 0, handle
+		cpi r16, 0
+		breq main_byte_done
+		; Rescan for break
+		rjmp main
+
+main_byte_a1:
+		;sbi PORTB, 4
+		;cbi PORTB, 4
+		out OCR0A, r17
+		rjmp main_byte_done
+
+main_byte_a2:
+		;sbi PORTB, 4
+		;cbi PORTB, 4
+		out OCR0B, r17
+		rjmp main_byte_done
+
+main_byte_done:
+		; Increase channel
+		adiw r25:r24, 1
+
+		;sbi PORTB, 4
+		;nop
+		;nop
+		;nop
+		;nop
+		;cbi PORTB, 4
+
+		; Assert stop bit or scan for new BREAK
+		sbic PINB, DMXPIN
+		rjmp main_read_byte
+		ldi r16, 60
+		rjmp main_rescan
+
+; FIXME in rescan, if nothing found default to full scan
 scan_break:
 		ldi r17, 0
 scan_break_1:
